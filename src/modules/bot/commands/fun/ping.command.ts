028aa85f5f6471894b9a3ba12f4091f733a185e2
Trung Nguyen Nguyen <nguyen.nguyen1512vlvl@gmail.com>
2023-08-06 21:39:23 +0700

    Feat: Add template slash command

---
+++ src/modules/bot/commands/fun/ping.command.ts
@@ -0,0 +1,19 @@
+import {
+  ChatInputCommandInteraction,
+  Interaction,
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+} from 'discord.js';
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('pong')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  execute(interaction: ChatInputCommandInteraction) {
+    interaction.reply({
+      content: `${Date.now() - interaction.createdTimestamp}ms`,
+      ephemeral: true,
+    });
+  },
+};
---
+++ src/modules/bot/events/interaction-create.event.ts
@@ -0,0 +1,17 @@
+import { Client, ClientEvents, Events, Interaction } from 'discord.js';
+import { CustomClient } from '../clients/custom-client';
+
+module.exports = {
+  name: Events.InteractionCreate as keyof ClientEvents,
+  execute(interaction: Interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const client = interaction.client as CustomClient;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) {
+      interaction.reply({ content: 'Outdated command' });
+    }
+
+    command.execute(interaction);
+  },
+};
