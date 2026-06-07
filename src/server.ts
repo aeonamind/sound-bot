75b72f38798aac7b62c82beb8d2acb2ce5e95ea2
eugene <aeonamind@gmail.com>
2026-06-07 19:07:19 +0700

    feat: update bot homepage

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { join } from "node:path";
+import { file } from "bun";
 import ejs from "ejs";
 import type { BotConfig } from "./config";
 
@@ -9,6 +10,13 @@ export function startServer(config: BotConfig) {
 		port: Number(process.env.PORT ?? 3000),
 		async fetch(req) {
 			const url = new URL(req.url);
+
+			if (url.pathname.startsWith("/images/") && req.method === "GET") {
+				const staticFile = file(join(viewsDir, url.pathname.slice(1)));
+				if (await staticFile.exists()) {
+					return new Response(staticFile);
+				}
+			}
 
 			if (url.pathname === "/" && req.method === "GET") {
 				const html = await ejs.renderFile(join(viewsDir, "index.ejs"), {
